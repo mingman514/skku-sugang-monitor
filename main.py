10beb90e7c8f9acbdf99d02613fdd6e6efb527bf
mingman514 <mingyuchoi514@gmail.com>
2022-02-25 08:50:38 +0900

    v1

---
+++ main.py
@@ -0,0 +1,57 @@
+from sugang_utils import *
+from datetime import datetime
+
+# 경로 설정에 따라 chromedriver.exe 파일이 같은 폴더 내에 있어야 함.
+"""
+To Fill Out
+"""
+Student_ID = '2021712971'
+Password = 'esteban514!'
+# 대상 전공과목 (학수번호-분반) 입력 - ex. 'SUP5003-41'
+Target_List = ['SUP5003-41', 'ADS5030-01']
+
+# 신청기간
+Start_Date = datetime.strptime('2022-02-22 08:00','%Y-%m-%d %H:%M')
+End_Date = datetime.strptime('2022-02-25 22:59','%Y-%m-%d %H:%M')
+
+# 신청시도 주기 (minute)
+# ex. At_Least = 1, At_Most = 5  :  1~5분 사이 랜덤시간마다 반복
+At_Least = 1
+At_Most = 5
+
+def is_register_period():
+    if Start_Date > End_Date:
+        print('신청기간 확인 필요')
+        exit()
+    Now = datetime.now()
+    if Start_Date <= Now and Now <= End_Date:
+        return True
+    return False
+
+if __name__ == '__main__':
+    set_ID(Student_ID)
+    set_PW(Password)
+    set_target_list(Target_List)   
+    # open page
+    access_to_webpage()
+    # login
+    sugang_page_login()
+    # move to 수강신청 tab
+    move_to_sugang_tab()
+    # check availability & register
+    while True:
+        # 수강신청 가능시간대 벗어나면 종료
+        if is_register_period():
+            available_subjects = check_available()
+
+            if len(available_subjects) > 0:
+                register(available_subjects)
+
+            wait_time = 60 * random.uniform(At_Least, At_Most)
+            print('{}초 대기 후에 모니터링 재개'.format(round(wait_time, 2)))
+            time.sleep(wait_time)
+
+        else:
+            print('신청 기간이 아니므로 종료')
+            exit()
+    
---
+++ sugang_utils.py
@@ -0,0 +1,142 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+import random, time
+from sys import exit
+
+sugang_id = ''
+sugang_pw = ''
+target_list = []
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome('chromedriver', chrome_options=options)
+
+def set_ID(ID):
+    global sugang_id
+    sugang_id = ID
+def set_PW(PW):
+    global sugang_pw
+    sugang_pw = PW
+def set_target_list(list):
+    global target_list
+    target_list = list
+
+def sleep_to_pretend_human():
+    sleeping_t = random.random() * 3
+    print('sleeping for {} sec'.format(sleeping_t))
+    time.sleep(sleeping_t)
+
+def access_to_webpage():
+    driver.maximize_window()
+    driver.get('https://sugang.skku.edu')
+
+    driver.implicitly_wait(15)
+    frame = driver.find_element_by_xpath('/html/body/iframe')
+    driver.switch_to.frame(frame)
+
+def sugang_page_login():
+
+    try:
+        id_input = driver.find_element_by_xpath('//*[@id="id"]')
+        pw_input = driver.find_element_by_xpath('//*[@id="pwd"]')
+        login_btn = driver.find_element_by_xpath('//*[@id="btn_login"]')
+        id_input.send_keys(sugang_id)
+        pw_input.send_keys(sugang_pw)
+        
+        sleep_to_pretend_human()
+        login_btn.click()
+        print('수강신청 페이지 로그인 성공')
+    except Exception:
+        print('수강신청 페이지 로그인 실패')
+        exit(1)
+
+
+def move_to_sugang_tab():
+    try:
+        driver.switch_to.default_content()
+        frame = driver.find_element_by_xpath('/html/body/iframe')
+        driver.switch_to.frame(frame)
+        frame = driver.find_element_by_xpath('//*[@id="contentFrame"]')
+        driver.switch_to.frame(frame)
+        frame = driver.find_element_by_xpath('//*[@id="topFrame"]')
+        driver.switch_to.frame(frame)
+    except Exception:
+        print('수강신청 탭 찾기 실패')
+        
+    sugang_tab = driver.find_element_by_xpath('//*[@id="cssmenu"]/ul/li[2]/a')
+    sleep_to_pretend_human()
+    sugang_tab.click()
+    time.sleep(5)
+
+def check_available():
+    try:
+        driver.switch_to.default_content()
+        frame = driver.find_element_by_xpath('/html/body/iframe')
+        driver.switch_to.frame(frame)
+        frame = driver.find_element_by_xpath('//*[@id="contentFrame"]')
+        driver.switch_to.frame(frame)
+        frame = driver.find_element_by_xpath('//*[@id="mainFrame"]')
+        driver.switch_to.frame(frame)
+    except Exception:
+        print('수강신청 목록 찾기 실패')
+
+    subject_rows = driver.find_elements_by_xpath('//*[@id="listLecture"]/tbody/tr')
+    subject_num = len(subject_rows) - 1
+    print('책가방에 담은 {}개 과목 확인'.format(subject_num))
+    subject_data = []
+
+    for i in range(subject_num):
+        tr = driver.find_element_by_xpath('//*[@id="{}"]'.format(i+1))
+        tr_data = []
+        for td in tr.find_elements_by_tag_name('td'):
+            tr_data.append(td.text)
+        subject_data.append(tr_data)
+        
+    for data in subject_data:
+        if data[2] not in target_list:
+            continue
+        elif data[1] != '신청완료':
+            break
+        print('모든 목표 과목에 대한 수강신청 완료!')
+        exit(1)
+    
+    # find target
+    # 전공과목 티오 기준임. (교양은 인덱스 수정 필요)
+    print('---- 신청 가능한 과목 ----')
+    target_idx = []
+    for idx, data in enumerate(subject_data):
+        if data[1] == '신청완료' or data[2] not in target_list:
+            continue
+        regist_num = int(data[8].split('/')[0])
+        wait_num = int(data[8].split('/')[1])
+        if regist_num < wait_num:
+            target_idx.append(idx)
+            print('{}번째 과목 - {}'.format(str(idx+1), data[3]))
+    print('-------------------------')
+
+    return target_idx
+
+def register(subjects_idx):
+    
+    for idx in subjects_idx:
+        try:
+            driver.switch_to.default_content()
+            frame = driver.find_element_by_xpath('/html/body/iframe')
+            driver.switch_to.frame(frame)
+            frame = driver.find_element_by_xpath('//*[@id="contentFrame"]')
+            driver.switch_to.frame(frame)
+            frame = driver.find_element_by_xpath('//*[@id="mainFrame"]')
+            driver.switch_to.frame(frame)
+        except Exception:
+            print('수강신청 목록 찾기 실패')
+            
+        driver.find_element_by_xpath('//*[@id="{}"]/td[2]/input[2]'.format(idx+1)).click()
+        time.sleep(3)
+        # repeat process
+        move_to_sugang_tab()
